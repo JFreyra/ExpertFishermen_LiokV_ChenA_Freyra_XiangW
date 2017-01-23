c5a6a688ad594cd8ed5cce781053a867c62581cc
Vincent Liok <vliok@stuy.edu>
2017-01-23 14:03:38 -0500

    bugs in deleteTask()

--- utils/todo.py
+++ utils/todo.py
@@ -22,10 +22,38 @@ def addTask( user_id, name, position ):
         counter-=1
         num+=1
     q = "INSERT INTO task VALUES( " + str(position) + ", " + str(user_id) + ", \"" + name + "\", " + str(position+1) + ")"
-    print q
     c.execute(q)
     db.commit()
     db.close()
     return True
 
-print addTask( 0, "sixthadded", 6 )
+def deleteTask( position ):
+    db=sqlite3.connect("../data/potato.db")
+    c=db.cursor()
+    q = "DELETE FROM task WHERE id = " + str(position)
+    c.execute(q)
+    q = "SELECT id FROM task"
+    c.execute(q)
+    infoid = c.fetchall()
+    if len(infoid) == 0:
+        lengthid = 0
+    else:
+        lengthid = len(infoid)
+    if position > lengthid or position < 0:
+        db.close()
+        return False
+    times = lengthid - position
+    counter = position
+    num = 0
+    print num
+    print times
+    while num < times:
+        q = "UPDATE task SET id = " + str(counter-1) + ", next_task_id = " + str(counter) + " WHERE id = " + str(counter)
+        c.execute(q)
+        counter+=1
+        num+=1
+    db.commit()
+    db.close()
+    return True
+
+print deleteTask(0)
